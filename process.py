f130508f3a6b731d5e09d16312ce0b15137eee9e
hrcarryu <hanrui22@outlook.com>
2023-04-15 10:51:25 +0800

    new logic

--- process.py
+++ process.py
@@ -3,50 +3,39 @@ import os
 import sys
 import json
 
-# Set up your OpenAI API key and model
-openai.api_key = "YOUR-API-KEY"
-model_engine = "gpt-3.5-turbo"  # Choose your preferred model
-model_engine = "GPT-4"
+openai.api_key = "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"
+model_engine = "gpt-3.5-turbo" # GPT-4
 
-# get file
-typename = sys.argv[1]
-filenames = os.listdir('Questions/'+typename+'/')
+type = sys.argv[1]
+folder = ""
+if type == "mzn":
+    folder = "minizinc-3.5"
+elif type == "py":
+    folder = "python-3.5"
+filenames = os.listdir('JSON')
 
 for file in filenames:
-    path = 'Questions/' + typename + '/' + file
+    path = f'JSON/{file}'
     with open(path, 'r') as load_f:
         load_dict = json.loads(load_f.read())
-        # print(load_dict)
-
-        # Define your prompt
-        prompt = load_dict['prompt']
-        # print(prompt)
-
-        # Set up the OpenAI API request
+        if type == 'mzn':
+            prompt = "The "+load_dict['name']+" problem is: "+load_dict['prompt']+" Build a MiniZinc model for the "+load_dict['name']+" problem. Please only output the model, without any prompt or explanation."
+        elif type == 'py':
+            prompt = "The "+load_dict['name']+" problem is: "+load_dict['prompt']+" Model the "+load_dict['name']+" problem using the Python API of OR-Tools. Please only output the code, without any prompt or explanation."
+        else:
+            prompt = load_dict['prompt']
+            
         response = openai.ChatCompletion.create(
             model = model_engine,
-            messages = [{"role": "user", "content": prompt}],
-            # temperature = 1,
-            # top_p = 1,
-            # n = 1,
-            # stream = false,
-            # stop = None,
-            # max_tokens = 1024,
-            # presence_penalty = 0,
-            # presence_penalty = 0,
-            # logit_bias = None,
-            # user = ""
+            messages = [{"role": "user", "content": prompt}]
         )
-
-        # print(response)
         message = response.choices[0].message.content.strip()
-
-        # ```
         begin = message.find('```')
         end = message.rfind('```')
-        code = message[begin+3:end]
-        # print(code)
+        code = message[begin:end].strip("```").strip("python")
 
-        with open('Answers/'+typename+'/'+file.split('.')[0]+'.'+typename, 'w') as write_f:
-            write_f.write(code)
-        print("write "+file+" done.")
+    with open(f'Prompts/{folder}/{file.split(".")[0]}.txt') as write_f:
+        write_f.write(prompt)
+    with open(f'Answers/{folder}/{file.split(".")[0]}.{type}') as write_f:
+        write_f.write(code)
+    print("write "+file+" done.")
