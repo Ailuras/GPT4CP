4c26fd775909c285aea2ef2fc5fef7c59115d225
hrcarryu <hanrui22@outlook.com>
2023-04-16 11:16:28 +0800

    2

--- SLC/python-4.0/49.py
+++ SLC/python-4.0/49.py
@@ -1,59 +1,32 @@
-from ortools.sat.python import cp_model
+from ortools.linear_solver import pywraplp
 
-
-def progressive_party():
-    model = cp_model.CpModel()
-
-    # Data
-    boats = range(7)
-    time_periods = range(6)
-    capacities = [4, 5, 4, 5, 4, 5, 999]
-    crew_sizes = [2, 2, 1, 1, 1, 1, 0]
+def number_partitioning(N):
+    solver = pywraplp.Solver.CreateSolver('GLOP')
 
     # Variables
-    host = [model.NewBoolVar(f"boat_{i}_host") for i in boats]
-    visit = {(i, j): [model.NewBoolVar(f"time_{t}_on_{i}_visiting_{j}")
-                       for t in time_periods]
-             for i in boats[:-1] for j in boats[:-1] if i != j}
+    x = []
+    for i in range(N):
+        x.append(solver.BoolVar(f'x_{i + 1}'))
 
     # Constraints
-    # Each guest must visit exactly one host
-    for j in boats[:-1]:
-        model.Add(sum(visit[(i, j)][t] for i in boats[:-1] if i != j
-                      for t in time_periods) == 1)
+    sum_x = solver.Sum(x[i] for i in range(N))
+    sum_x_squared = solver.Sum((i + 1) * (i + 1) * x[i] for i in range(N))
 
-    # Each host must be visited by at least one guest
-    for i in boats[:-1]:
-        model.Add(sum(visit[(i, j)][t] for j in boats[:-1] if i != j
-                      for t in time_periods) >= host[i])
+    solver.Add(sum_x == N // 2)
+    solver.Add(sum_x_squared == N * (N + 1) * (2 * N + 1) // 8)
 
-    # Guest boats can only visit hosts once
-    for i in boats[:-1]:
-        for j in boats[:-1]:
-            if i != j:
-                for t in time_periods:
-                    model.Add(visit[(i, j)][t] + visit[(j, i)][t] <= 1)
+    # Objective function
+    solver.Minimize(sum_x)
 
-    # Capacity constraints
-    for i in boats:
-        for t in time_periods:
-            model.Add(sum(crew_sizes[j]*visit[(j, i)][t]
-                          for j in boats[:-1] if j != i) <= capacities[i])
+    status = solver.Solve()
 
-    # Objective
-    model.Minimize(sum(host))
+    if status == pywraplp.Solver.OPTIMAL:
+        A = [i + 1 for i in range(N) if x[i].solution_value() == 1]
+        B = [i + 1 for i in range(N) if x[i].solution_value() == 0]
+        return A, B
+    else:
+        return None, None
 
-    # Solve
-    solver = cp_model.CpSolver()
-    solver.Solve(model)
-
-    # Print solution
-    for i in boats[:-1]:
-        print(f"Boat {i}: Host={solver.Value(host[i])}\tVisiting=", end="")
-        for j in boats[:-1]:
-            if i != j and solver.Value(visit[(i, j)][0]) == 1:
-                print(f"{j}", end="")
-        print()
-
-
-progressive_party()
+N = 6
+A, B = number_partitioning(N)
+print(A, B)
